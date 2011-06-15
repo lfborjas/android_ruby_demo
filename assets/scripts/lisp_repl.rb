8a5216f5a9132eef02192ce027af513ceda7e92e
lfborjas <luis.borjas@escolarea.com>
2011-06-14 22:54:18 -0400

    ya salen las cosas, pero truena el click

--- assets/scripts/lisp_repl.rb
+++ assets/scripts/lisp_repl.rb
@@ -1,12 +1,19 @@
 require 'ruboto'
 
-ruboto_import_widgets :EditText, :TextView, :Button
+ruboto_import_widgets :EditText, :TextView, :Button, :LinearLayout
 
 $activity.handle_create do |b|
     setTitle "Scheme Repl" 
 
     setup_content do
-        @et = edit_text :single_line => true, :hint => "Enter a sexpr"
+        linear_layout :orientation => LinearLayout::VERTICAL do
+            @et = edit_text :single_line => true, :hint => "Enter a sexpr"
+            button :text => "Eval", :on_click_listener => lambda{|v| eval_sexpr }
+            @tv = text_view :text => "Gimme some sexprs!"
+        end
     end
 
+    def self.eval_sexpr(expr)
+        @tv.append "\n #{expr}"    
+    end
 end
