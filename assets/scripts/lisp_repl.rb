12de7159d9832ee89c83e5360f8d09dd765a186d
lfborjas <luis.borjas@escolarea.com>
2011-06-14 23:02:40 -0400

    managing to put *some* text in the textview

--- assets/scripts/lisp_repl.rb
+++ assets/scripts/lisp_repl.rb
@@ -8,12 +8,12 @@ $activity.handle_create do |b|
     setup_content do
         linear_layout :orientation => LinearLayout::VERTICAL do
             @et = edit_text :single_line => true, :hint => "Enter a sexpr"
-            button :text => "Eval", :on_click_listener => lambda{|v| eval_sexpr }
+            button :text => "Eval"
             @tv = text_view :text => "Gimme some sexprs!"
         end
     end
 
-    def self.eval_sexpr(expr)
-        @tv.append "\n #{expr}"    
+    handle_click do |view|
+        @tv.append "\n #{view.getText}"
     end
 end
